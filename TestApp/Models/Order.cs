00df28bd1fccbeaf5dd92062edfbab1ea01cd7b6
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-12 21:27:37 +0300

    Discribed DB model in Order.cs including Required fields

---
+++ TestApp/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestApp.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    [Required]
+    public string SenderCity { get; set; } = string.Empty;
+    [Required]
+    public string SenderAddress { get; set; } = string.Empty;
+    [Required]
+    public string ReceiverCity { get; set; } = string.Empty;
+    [Required]
+    public string ReceiverAddress { get; set; } = string.Empty;
+    [Required]
+    public decimal Weight { get; set; }
+    [Required]
+    public DateOnly OrderPickupDate { get; set;}
+}
