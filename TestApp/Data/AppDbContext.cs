f5bd41db773994c1282f3053b782a471e4c29ea0
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-12 22:00:06 +0300

    Added AppDbContext.cs

---
+++ TestApp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApp.Models;
+
+namespace TestApp.Data;
+
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext (DbContextOptions<AppDbContext> options) :  base (options){}
+    
+    public DbSet<Order> Orders => Set<Order>();
+}
