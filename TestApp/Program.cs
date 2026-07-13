77eec9306a36e020330401a5844442852880ed50
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-13 22:55:20 +0300

    Program.cs updated to support swagger

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -7,9 +7,42 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "TestApp API v1");
+        options.RoutePrefix = "swagger";
+    });
+    app.MapOpenApi();
+}
+
 app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseExceptionHandler("/error");
+
 app.MapControllers();
+
 app.Run();
