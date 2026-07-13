893442052f919c1ffddb1b50727beeb6c07e08b4
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-13 20:09:23 +0300

    New controller, maybe final version

--- TestApp/Controllers/OrderController.cs
+++ TestApp/Controllers/OrderController.cs
@@ -1,6 +1,61 @@
-﻿namespace TestApp.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestApp.Data;
+using TestApp.Models;
+using TestApp.Dtos;
 
-public class OrderController
+namespace TestApp.Controllers;
+[ApiController]
+[Route("api/orders")]
+public class OrderController : ControllerBase
 {
+    private readonly AppDbContext _context;
+
+    public OrderController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    private static OrderDto ToDto(Order order) => new()
+    {
+        Id = order.Id,
+        SenderCity = order.SenderCity,
+        SenderAddress = order.SenderAddress,
+        ReceiverCity = order.ReceiverCity,
+        ReceiverAddress = order.ReceiverAddress,
+        Weight = order.Weight,
+        OrderPickupDate = order.OrderPickupDate
+    };
     
+    [HttpGet]
+    public async Task<IActionResult> GetOrders()
+    {
+        var orders = await _context.Orders.OrderByDescending(x => x.Id).Select(order => ToDto(order)).ToListAsync();
+        return Ok(orders);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetOrderById(int id)
+    {
+        var order = await _context.Orders.FirstOrDefaultAsync(x => x.Id == id);
+        if (order == null) return NotFound();
+        return Ok(ToDto(order));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder(CreateOrderDto dto)
+    {
+        var order = new Order()
+        {
+            SenderAddress = dto.SenderAddress,
+            SenderCity = dto.SenderCity,
+            ReceiverAddress = dto.ReceiverAddress,
+            ReceiverCity = dto.ReceiverCity,
+            Weight = dto.Weight,
+            OrderPickupDate = dto.OrderPickupDate,
+        };
+        _context.Orders.Add(order);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetOrderById), new { id = order.Id }, ToDto(order));
+    }
 }
